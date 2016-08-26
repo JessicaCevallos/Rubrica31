aab2caed9b0ea3ef440a89ad10991ac7654916f0
Jessica Cevallos <Jessica Cevallos@Jessica Cevallos>
2016-08-26 12:35:02 -0500

    2 Commit

--- Tarea de ingenieria 2/src/tarea/de/ingenieria/pkg2/TareaDeIngenieria2.java
+++ Tarea de ingenieria 2/src/tarea/de/ingenieria/pkg2/TareaDeIngenieria2.java
@@ -17,6 +17,7 @@ public class TareaDeIngenieria2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Bienvenido a java");
+        System.out.println(" es fácil");
     }
     
 }
